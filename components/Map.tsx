0961b10587132d0d34352592505ce40d1997b53f
Faryad Azim <faryadmaalic92@yahoo.com>
2023-04-28 18:21:34 +0500

    remove iframe ref of google map location

--- components/Map.tsx
+++ components/Map.tsx
@@ -5,7 +5,7 @@ const Map = () => {
   return (
     <>
 
-      <iframe className="rounded-2xl" width={736} height={357} frameBorder={0} scrolling="no" marginHeight={0} marginWidth={0} src="https://maps.google.com/maps?width=736&height=357&hl=en&q=burj%20khalifa%20+(building)&t=h&z=9&ie=UTF8&iwloc=B&output=embed">&lt;a href="https://www.maps.ie/distance-area-calculator.html"&gt;area maps&lt;/a&gt;</iframe>
+      <iframe className="rounded-2xl" width={736} height={357}  src="https://maps.google.com/maps?width=736&height=357&hl=en&q=burj%20khalifa%20+(building)&t=h&z=9&ie=UTF8&iwloc=B&output=embed"></iframe>
 
     </>
   )
