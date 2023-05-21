6bd74c2a37637ba3cc8dd78c76c2c4733665b95a
Faryad Azim <faryadmaalic92@yahoo.com>
2023-05-22 00:52:21 +0500

    Added Marketplace ,a user-friendly page listing all available marketplace items

--- app/marketplace/page.tsx
+++ app/marketplace/page.tsx
@@ -31,27 +31,25 @@ export default async function MarketplacePage() {
 
   return (
     <div className="bg-customlightGrey">
-      <section className={`hero-section relative  bg-customDarkGreen pb-10  ${inter.className} z-30`}>
-        <div className={`flex   flex-col sticky top-0 z-50`}>
-          <header className="container  border-t-pink-0 border-l-pink-0 border-t-pink-0   top-0 z-40">
 
-            {/* navigation  */}
-            <div className="flex h-16 items-center justify-between pt-7" >
+
+      <section className={`hero-section relative  bg-customDarkGreen   ${inter.className} z-30`}>
+        <div className={`flex   flex-col sticky top-0 z-50`}>
+          <header className="container   top-0 z-48">
+            <div className="flex h-16 items-center justify-between py-5" >
               <MainNav items={marketingConfig.mainNav} />
               <nav className="flex">
-                <Link href="/blogs" className={cn("flex items-center text-lg font-bold text-slate-600 sm:text-sm text-white tracking-widest mx-3 hidden md:flex")}>Blogs</Link>
-                <Link href="/contactus" className={cn("flex items-center text-lg font-bold text-slate-600 sm:text-sm text-white tracking-widest mx-3 hidden  md:flex ")}>CONTACT US</Link>
-                <Link href="/dashboard" className={cn(buttonVariants({ size: "sm", variant: "outline" }), "  text-white w-100 px-10 rounded-full font-bold")}>LOGIN</Link>
+                <Link href="/contact-us" className={cn("  items-center text-lg   sm:text-sm text-white tracking-widest mx-3 hidden  lg:flex text-[15px]  font-normal  ")} style={{ fontFamily: "Basier Square Regular" }}>Contact Us</Link>
+                <Link href="/dashboard" className={cn(buttonVariants({ size: "sm", variant: "outline" }), "px-4 text-white w-100 px-10 rounded-full font-normal")} style={{ fontFamily: "Basier Square Regular" }}>Login</Link>
               </nav>
             </div>
           </header>
-          <div className=" my-5 ">
-            <Separator className=" bg-white" />
-          </div>
 
-          <div className="container flex justify-between ">
+
+
+          <div className="container flex justify-between my-10 ">
             <div className="opertunityInfo">
-              <h3 className="not-italic font-semibold text-4xl flex items-center text-white" style={{ fontFamily: "sans-serif" }} id="oppertunityTitle">
+              <h3 className=" font-bold  sm:text-2xl text-3xl md:text-[34px] flex items-center text-white" style={{ fontFamily: "sans-serif" }} id="oppertunityTitle">
                 Marketplace
               </h3>
 
@@ -59,30 +57,36 @@ export default async function MarketplacePage() {
 
             </div>
 
+
           </div>
 
 
         </div>
       </section>
 
-
       <section className="container my-6">
         <div className="header flex  mt-20">
 
-          <div className="w-1/3">    <Input placeholder="Search for Lands" className="" /></div>
-          <div className="w-1/3 text-right">Showing 10 out of 1,234 results</div>
-          <div className="w-1/3 text-right  flex justify-end">
-            <Select>
-              <SelectTrigger className="w-[180px]">
-                <SelectValue placeholder="Select a Country" />
+          <div className="w-1/2 md:w-1/3  h-9  flex items-center">    <Input placeholder="Search for Lands" className="  bg-white w-80" /></div>
+          <div className="w-1/3  flex h-9   justify-end items-center  text-right font-normal text-[17px] text-black ">
+
+            <span className="hidden lg:flex">
+
+              Showing 10 out of 1,234 results
+            </span>
+          </div>
+          <div className="w-1/2 md:w-1/3 text-right  flex justify-end items-center h-9 ">
+            <Select >
+              <SelectTrigger className="w-[180px] bg-white rounded-lg  ">
+                <SelectValue placeholder="Country" />
               </SelectTrigger>
               <SelectContent>
                 <SelectGroup>
                   <SelectItem value="grapes">Pakistan</SelectItem>
-                  <SelectLabel>UAE</SelectLabel>
+                  {/* <SelectLabel>UAE</SelectLabel> */}
                   <SelectItem value="apple">Oman</SelectItem>
                   <SelectItem value="banana">Egypt</SelectItem>
-                  <SelectItem value="blueberry">India</SelectItem>
+                  <SelectItem value="blueberry">United Kingdom</SelectItem>
                   <SelectItem value="pineapple">USA</SelectItem>
                 </SelectGroup>
               </SelectContent>
@@ -91,70 +95,77 @@ export default async function MarketplacePage() {
 
         </div>
 
-        <div className="flex flex-wrap  mt-6">
+        <div className="flex flex-wrap   mt-6 justify-between">
           {[1, 2, 3, 5, 6, 7, 8, 9, 4].map((x: any): any => {
             return (
-              <div className="md:w-4/12  my-2 md:my-2" key={x}>
-                <div className="bg-white shadow-lg   rounded-2xl  h-100  mx-2 ">
+              <div className="md:w-[32%] w-full  my-2 mb-3" key={x}>
+                <div className="bg-white  shadow-md  rounded-2xl  h-100    ">
                   {/* <div className="bg-cover bg-center h-80" style={{ backgroundImage: "url('/images/Diversification.png')" }}></div> */}
                   <div className="relative h-[179px] text-center flex justify-center  ">
                     <Image
                       src="/images/Img.png"
                       alt="Image"
-                      // fill
-                      width={384}
-                      height={179}
+                      fill
+                      // width={384}
+                      // height={179}
                       className="rounded-t-2xl"
                     // style={{ objectFit: "cover", objectPosition: "center" }}
                     />
                   </div>
-                  <div className="p-4">
+                  <div className="lg:p-6 p-4">
                     <div className="flex justify-between">
-                      <span>Bradley,CA</span>
-                      <span>2000 SQFT</span>
+                      <span className="font-normal text-[13px]">Bradley,CA</span>
+                      <span className="font-normal text-[13px]">2000 SQFT</span>
                     </div>
-                    <h2 className="text-xl font-bold mb-2">Lynch Canyon Road</h2>
-                    <div className="flex justify-start">
-                      <span className="price   font-semibold text-base leading-5 text-black">USD,1,023,550</span>
+                    <h2 className="text-xl font-bold mb-3 ">Lynch Canyon Road</h2>
+                    <div className="flex justify-between">
+
+                      <span className="price  md:mb-0 mb-1  font-bold text-base leading-5 text-black">$ 1,023,550</span>
                     </div>
+
+
                     <div className="flex justify-center">
-                      <Link href="/opportunities" className={cn(buttonVariants({ variant: "primaryDark" }), "  mr-2   w-full px-6 rounded-full font-bold  py-6 my-2")} style={{ fontFamily: "sans-serif" }}>Contact Seller</Link>
+                      <Link href="/opportunities" className={cn(buttonVariants({ variant: "primaryDark" }), "  mr-2  w-full text-center lg:w-[90%]  px-6 rounded-full font-bold lg:font-semibold md:font-normal    py-6  md:my-1 lg:my-2  ")} style={{ fontFamily: "sans-serif" }}>Contact Seller</Link>
                     </div>
 
                   </div>
                 </div>
               </div>
+
+
             )
           })}
         </div>
 
-        <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-end my-5">
+        <div className="  flex  justify-center items-center md:justify-end my-5">
 
-          <div>
-            <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
-              <a href="#" className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 bg-white">
 
-                <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                  <path fillRule="evenodd" d="M12.79 5.23a.75.75 0 01-.02 1.06L8.832 10l3.938 3.71a.75.75 0 11-1.04 1.08l-4.5-4.25a.75.75 0 010-1.08l4.5-4.25a.75.75 0 011.06.02z" clipRule="evenodd" />
-                </svg>
-                <span className="text-black px-2">Previous</span>
-              </a>
-              {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
-              <a href="#" aria-current="page" className="relative z-10 inline-flex items-center bg-customDarkGreen text-white px-4 py-2 text-sm font-semibold  focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ">1</a>
-              <a href="#" className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0  bg-white">2</a>
-              <a href="#" className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0  bg-white">3</a>
-              <a href="#" className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0  bg-white">4</a>
-              <a href="#" className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0  bg-white">5</a>
-              <a href="#" className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0  bg-white">6</a>
-              <a href="#" className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0  bg-white">7</a>
-              <a href="#" className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0  bg-white">
-                <span className="text-black px-3">Next</span>
-                <svg className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                  <path fillRule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clipRule="evenodd" />
-                </svg>
-              </a>
-            </nav>
-          </div>
+          <nav className="isolate inline-flex -space-x-px rounded-md shadow-sm  " aria-label="Pagination">
+            <a href="#" className="relative inline-flex items-center rounded-l-md px-2 py-2 pl-4 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 bg-white">
+
+              <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M15.8327 9.99996H4.16602M4.16602 9.99996L9.99935 15.8333M4.16602 9.99996L9.99935 4.16663" stroke="#344054" stroke-width="1.67" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
+              <span className="text-black px-2 hidden md:flex">Previous</span>
+            </a>
+            {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
+            <a href="#" aria-current="page" className="relative z-10 inline-flex items-center bg-customDarkGreen text-white px-4 py-2 text-sm font-semibold  focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 ">1</a>
+            <a href="#" className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0  bg-white">2</a>
+            <a href="#" className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0  bg-white">3</a>
+            <a href="#" className="relative hidden md:inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0  bg-white ">4</a>
+            <a href="#" className="relative hidden md:inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0  bg-white ">5</a>
+            <a href="#" className="relative hidden md:inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0  bg-white">6</a>
+            <a href="#" className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0  bg-white">7</a>
+            <a href="#" className="relative inline-flex items-center rounded-r-md px-2 py-2  pr-4  text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0  bg-white">
+              <span className="text-black px-3 hidden md:flex">Next</span>
+
+              <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M4.16602 9.99996H15.8327M15.8327 9.99996L9.99935 4.16663M15.8327 9.99996L9.99935 15.8333" stroke="#344054" stroke-width="1.67" stroke-linecap="round" stroke-linejoin="round" />
+              </svg>
+
+            </a>
+          </nav>
+
         </div>
 
       </section>
