b3d19a7b58458117e03586ae0ce9727bfadba76c
Faryad Azim <faryadmaalic92@yahoo.com>
2023-05-24 16:24:00 +0500

    Making Navbar Dynamic for Dark Light mode ,

--- app/dashboard/addfund/loading.tsx
+++ app/dashboard/addfund/loading.tsx
@@ -2,6 +2,18 @@
 
 export default function DashboardLoading() {
   return (
-    <>loading page</>
+    <div className="w-screen h-screen flex justify-center items-center">
+    <div className="flex">
+      <div className="relative">
+        {/* Outer Ring*/}
+        <div className="w-12 h-12 rounded-full absolute
+                  border border-solid border-gray-200" />
+        {/* Inner Ring */}
+        <div className="w-12 h-12 rounded-full animate-spin absolute
+                  border border-solid  border-[#70F09C]  border-t-transparent" />
+      </div>
+    </div>
+
+  </div >
   )
 }
