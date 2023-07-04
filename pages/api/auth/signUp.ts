b8b8efd3b3bf6023dd8c73ae5658affd37b341ba
muhammadilyas <muhammadilyas@intensivate.com>
2023-07-04 12:41:29 +0500

    changes

--- pages/api/auth/signUp.ts
+++ pages/api/auth/signUp.ts
@@ -1,6 +1,9 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import supabase from "../../../supabase";
 import { sendOTP } from "@/functions/sendOTP";
+import speakeasy from "speakeasy";
+import sendEmail from "@/functions/sendEmail";
+import { signUpTemplate } from "@/emailTemplates/signUp";
 
 type Data = {
   message: string;
@@ -54,7 +57,38 @@ export default async function handler(
               throw new Error(insertDataError.message);
             }
             // send OTP
-            sendOTP(email, userId);
+            // sendOTP(email, userId);
+            const secret = speakeasy.generateSecret({ length: 20 });
+            const secretBase32 = secret.base32;
+
+            const verificationCode = speakeasy.totp({
+              secret: secretBase32,
+              encoding: "base32",
+              digits: 5, // Number of digits for the verification code
+            });
+            const currentDate = new Date();
+            const expirationDate = new Date(
+              currentDate.getTime() + 24 * 60 * 60 * 1000
+            );
+            // save otp data to database.
+            const { data: insertOtp, error: insertOtpError } = await supabase
+              .from("otpCodes")
+              .insert({
+                email,
+                otp: verificationCode,
+                expired_at: expirationDate,
+                userId: userId,
+              });
+            if (insertOtpError) {
+              throw new Error(insertOtpError.message);
+            }
+            const mailResponse = sendEmail(email, "Verify Email", () =>
+              signUpTemplate(verificationCode)
+            );
+
+            if ((await mailResponse).status !== "200") {
+              throw new Error("Failed to send OTP");
+            }
           })
           .catch((error) => {
             throw new Error(error.message);
