6629c297eecb462d8ffa59fc001a656e6bd96ee4
muhammadilyas <muhammadilyas@intensivate.com>
2023-07-10 14:11:02 +0500

    changes

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 const Map = ({ src }: any) => {
   const styledCode = src.replace(/(style|allowfullscreen)="[^"]*"/g, "");
-  const modifiedCode = styledCode.replace(
+  const modifiedCode = styledCode?.replace(
     "></iframe>",
     `style="border-radius: 1rem; width: 100%; height: 357px;" ></iframe>`
   );
