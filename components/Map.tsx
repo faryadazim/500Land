2666c63b0aa37c846df28d2e074c21ab6ac5b16e
muhammadilyas <muhammadilyas@intensivate.com>
2023-07-10 14:13:55 +0500

    changes

--- components/Map.tsx
+++ components/Map.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 const Map = ({ src }: any) => {
   const styledCode = src.replace(/(style|allowfullscreen)="[^"]*"/g, "");
-  const modifiedCode = styledCode?.replace(
-    "></iframe>",
-    `style="border-radius: 1rem; width: 100%; height: 357px;" ></iframe>`
-  );
+  // const modifiedCode = styledCode?.replace(
+  //   "></iframe>",
+  //   `style="border-radius: 1rem; width: 100%; height: 357px;" ></iframe>`
+  // );
 
   return (
-    <>{src && <div dangerouslySetInnerHTML={{ __html: modifiedCode }}></div>}</>
+    <>{src && <div dangerouslySetInnerHTML={{ __html: styledCode }}></div>}</>
   );
 };
 
